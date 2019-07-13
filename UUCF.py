808784daf55a0769ef002e840de59277271f7e72
Ghaith Arfaoui <GhaithArf@users.noreply.github.com>
2019-07-13 16:03:39 +0200

    clean and comment UUCF.py

--- UUCF.py
+++ UUCF.py
@@ -1,12 +1,10 @@
 import pandas as pd
 import numpy as np
 from scipy import sparse as sp
-from scipy.sparse.linalg import norm
-import sklearn.preprocessing as pp
 
 
 class UUCFRecommender:
-    UU = {}  ## user-user similarities; constructed lazily
+    UU = {}  # session-session similarities dict
 
     def __init__(self, with_abs_sim=True, with_deviations=True, k=5, ratings=None, R=None, session_id_labels=None, session_id_levels=None, reference_labels=None,
                               reference_levels=None):
@@ -14,7 +12,6 @@ class UUCFRecommender:
         self.with_deviations = with_deviations
         self.k = 5
         self.ratings = ratings
-        self.dataframe = ratings
         self.R = R
         self.session_id_labels = session_id_labels
         self.session_id_levels = session_id_levels
@@ -23,12 +20,14 @@ class UUCFRecommender:
 
 
     def create_Ratings_Matrix(self):
+        """
+            Create the rating matrix and assign a reference id for each session_id and item id (reference)
+        """
 
-
-        self.items = self.dataframe['reference'].unique()
+        self.items = self.ratings['reference'].unique()
         self.items.sort()
 
-        self.session_ids = self.dataframe['session_id'].unique()
+        self.session_ids = self.ratings['session_id'].unique()
         self.session_ids.sort()
 
         self.m = self.session_ids.size
@@ -44,26 +43,48 @@ class UUCFRecommender:
                                                          self.ratings['reference'].map(self.itemId_to_itemIDX))))
         self.R_dok = self.R.todok()
 
-    def compute_user_avgs(self):
+    def compute_session_avgs(self):
+        """
+            Computes the average rating of each session
+        """
+
         session_sums = self.R.sum(axis=1).A1  ## matrix converted to 1-D array via .A1
         self.session_cnts = (self.R != 0).sum(axis=1).A1
         self.session_avgs = session_sums / self.session_cnts
 
-    def compute_pairwise_session_similarity(self, u_id, v_id):
+    def compute_pairwise_session_similarity(self, s1_id, s2_id):
 
-        u = self.R[u_id, :].copy()
-        v = self.R[v_id, :].copy()
+        """
+           Computes the mean-centered cosine similarity between two sessions
 
-        # normalize values with user rating mean
-        u.data = u.data - self.session_avgs[u_id]
-        v.data = v.data - self.session_avgs[v_id]
-        numerator = u.dot(v.T)
+           Parameters
+           ----------
+            s1_id: str
+                ID corresponding to session 1
+            s2_id: str
+                ID corresponding to session 2
 
-        # calculate sum of squares
-        ssq_u = np.sum(u.data ** 2)
-        ssq_v = np.sum(v.data ** 2)
+           Returns
+           -------
+           Similarity: Float
+                Similarity value between session 1 and session 2
 
-        denominator = np.sqrt(ssq_u * ssq_v)
+        """
+
+        # Copy the vector corresponding to s1 and s2
+        s1 = self.R[s1_id, :].copy()
+        s2 = self.R[s2_id, :].copy()
+
+        # normalize values with session weight mean
+        s1.data = s1.data - self.session_avgs[s1_id]
+        s2.data = s2.data - self.session_avgs[s2_id]
+        numerator = s1.dot(s2.T)
+
+        # calculate sum of squares for s1 and s2
+        ssq_s1 = np.sum(s1.data ** 2)
+        ssq_s2 = np.sum(s2.data ** 2)
+
+        denominator = np.sqrt(ssq_s1 * ssq_s2)
 
         if denominator == 0:
             similarity = 0.;
@@ -72,92 +93,118 @@ class UUCFRecommender:
 
         return similarity
 
-    def compute_session_similarities(self, u_id):
-        if u_id in self.UU.keys():  ## persist
+    def compute_session_similarities(self, s_id):
+
+        """
+           Computes the mean-centered cosine similarity between two sessions
+
+           Parameters
+           ----------
+            s1_id: str
+                ID corresponding to the session
+        """
+
+        if s_id in self.UU.keys():
             return
 
-        uU = np.empty((self.m,))
+        sS = np.empty((self.m,))
 
-        ########## BEGIN HERE ##########
         nnz_per_row = np.diff(self.R.indptr)
         avg_array = np.repeat(self.session_avgs, nnz_per_row)
         R_copy = self.R.copy()
         R_copy.data -= avg_array
 
-        u = R_copy[u_id, :].copy()
+        s1 = R_copy[s_id, :].copy()
 
-        numerator = R_copy.dot(u.T)
+        numerator = R_copy.dot(s1.T)
 
-        # calculate sum of squares
-        ssq_u = np.sum(u.data ** 2)
+        ssq_s1 = np.sum(s1.data ** 2)
 
         R_copy.data = R_copy.data ** 2
-        ssq_v = R_copy.sum(axis=1)
+        ssq_s2 = R_copy.sum(axis=1)
 
-        denominator = np.sqrt(ssq_v.dot(ssq_u))
+        denominator = np.sqrt(ssq_s2.dot(ssq_s1))
 
-        uU = numerator / denominator;
-        uU = np.nan_to_num(uU)
-        ##########  END HERE  ##########
+        sS = numerator / denominator;
+        sS = np.nan_to_num(sS)
+
+        self.UU[s_id] = sS
+
+    def create_session_neighborhood(self, s_id, i_id):
 
         """
-        ########## BEGIN BONUS ##########
+           Creates the neighborhood of a given session
 
-        ##########  END BONUS  ##########
+           Parameters
+           ----------
+            s_id: str
+                ID corresponding to the session
+            i_id: str
+                ID corresponding to the item
+
+            Returns
+            -------
+            nh: dict
+                Neighborhood corresponding to the session s_id
         """
 
-        self.UU[u_id] = uU
+        nh = {}  # Initialize the beighborhood dict
 
-    def create_user_neighborhood(self, u_id, i_id):
-        nh = {}  ## the neighborhood dict with (user id: similarity) entries
-        ## nh should not contain u_id and only include users that have rated i_id; there should be at most k neighbors
-        self.compute_session_similarities(u_id)
-        uU = self.UU[u_id].copy()
+        self.compute_session_similarities(s_id)
+        uU = self.UU[s_id].copy()
 
-        uU_copy = uU.copy()  ## so that we can modify it, but also keep the original
+        uU_copy = uU.copy()
 
-        ########## BEGIN HERE ##########
         i = 0
-        userArray = np.squeeze(np.asarray(uU))
-        argArray = np.argsort(userArray)[::-1]
+        SessionArray = np.squeeze(np.asarray(uU))
+        argArray = np.argsort(SessionArray)[::-1]
 
-        # remove user from userlist
-        index = np.argwhere(argArray == u_id)
+        # remove the session from the sessions list
+        index = np.argwhere(argArray == s_id)
         argArray = np.delete(argArray, index)
 
         if self.with_abs_sim == False:
             for x in argArray:
                 if (x, i_id) in self.R_dok:
-                    nh[x] = userArray[x]
+                    nh[x] = SessionArray[x]
                     i += 1
                 if i == self.k:
                     break
         else:
-            tempArray = np.absolute(userArray)
+            tempArray = np.absolute(SessionArray)
             absArray = np.argsort(tempArray)[::-1]
             for x in absArray:
                 if (x, i_id) in self.R_dok:
-                    nh[x] = userArray[x]
+                    nh[x] = SessionArray[x]
                     i += 1
                 if i == self.k:
                     break
-        ##########  END HERE  ##########
 
         return nh
 
-    def predict_rating(self, u_id, i_id):
+    def predict_rating(self, s_id, i_id):
 
-        #         if (u_id, i_id) in self.R_dok:
-        #             print("user", u_id, "has rated item", i_id, "with", self.R[u_id, i_id])
-        #         else:
-        #             print("user", u_id, "has not rated item", i_id)
-        #         print("k:", self.k, "with_deviations:", self.with_deviations, "with_abs_sim:", self.with_abs_sim)
+        """
+        Predicts the rating session s_id would give to item i_id
 
-        nh = self.create_user_neighborhood(u_id, i_id)
+        Parameters
+        ----------
+        s_id: str
+           ID corresponding to the session
+        i_id: str
+           ID corresponding to the item
+
+        Returns
+        -------
+        prediction: float
+           Predicted rating
+        """
+
+
+        nh = self.create_session_neighborhood(s_id, i_id)
 
         neighborhood_weighted_avg = 0.
 
-        ########## BEGIN HERE ##########
         sumRatings = 0
         sumAbsWeights = 0
         for n_id, w in nh.items():
@@ -172,25 +219,35 @@ class UUCFRecommender:
             neighborhood_weighted_avg = 0;
         else:
             neighborhood_weighted_avg = sumRatings / sumAbsWeights
-        ##########  END HERE  ##########
-
-        # if sum_weights != 0: ## avoid division by zero
-        #    neighborhood_weighted_avg = sum_scores/sum_weights
 
         if self.with_deviations:
-            prediction = self.session_avgs[u_id] + neighborhood_weighted_avg
-        #             print("prediction ", prediction, " (user_avg ", self.user_avgs[u_id], " offset ", neighborhood_weighted_avg, ")", sep="")
+            prediction = self.session_avgs[s_id] + neighborhood_weighted_avg
         else:
             prediction = neighborhood_weighted_avg
-        #             print("prediction ", prediction, " (user_avg ", self.user_avgs[u_id], ")", sep="")
-
         return prediction
 
+
     def recommend(self, session_id, item_ids=None, topN=20):
+        """
+        Recomment topN items to a session
 
-        movies_rated_by_user = self.ratings[self.ratings['session_id'] == session_id]['reference'].tolist()
+        Parameters
+        ----------
+        session_id: str
+           ID corresponding to the session
+        topN: int
+           Maximum number of items to recommend
+        item_ids: list
+            list of items to consider when recommending
 
-        u_id = self.sessionId_to_sessionIDX[session_id]
+        Returns
+        -------
+        recommendations: list
+           List of item recommendations
+        """
+        items_viewed_in_session = self.ratings[self.ratings['session_id'] == session_id]['reference'].tolist()
+
+        s_id = self.sessionId_to_sessionIDX[session_id]
 
         recommendations = []
 
@@ -198,13 +255,13 @@ class UUCFRecommender:
             item_ids = self.items
 
         for item_id in item_ids:
-            if item_id in movies_rated_by_user:
+            if item_id in items_viewed_in_session:
                 continue
             i_id = self.itemId_to_itemIDX[item_id]
 
-            rating = self.predict_rating(u_id, i_id)
+            rating = self.predict_rating(s_id, i_id)
             recommendations.append((item_id, rating))
 
         recommendations = sorted(recommendations, key=lambda x: -x[1])[:topN]
-        #         print(recommendations)
+
         return [item_id for item_id, rating in recommendations]
